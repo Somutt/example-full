98d5ce8332bd52627043195e3cb7539e47aeaf35
Samuel Moura Alves <samuelmalves1@gmail.com>
2023-08-24 17:50:49 -0300

    updated test example

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,4 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/.out/', '/public/'],
-
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
 };
