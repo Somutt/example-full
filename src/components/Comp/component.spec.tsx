352f8c9118adc6174057be018b1b2882cc37eca0
Samuel Moura Alves <samuelmalves1@gmail.com>
2023-08-24 17:51:35 -0300

    updated test example 2

---
+++ src/components/Comp/component.spec.tsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import 'jest-styled-components';
+import { screen } from '@storybook/testing-library';
+import { renderTheme } from '../../utils/renderTheme';
+import { Comp } from '.';
+
+describe('<Comp />', () => {
+  it('Should render Comp correctly with title', () => {
+    renderTheme(<Comp title='oi' />);
+    const heading = screen.getByRole('heading', { name: /oi/i });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Should render Comp correctly without title', () => {
+    renderTheme(<Comp />);
+    const heading = screen.queryByRole('heading', { name: /oi/i });
+    expect(heading).not.toBeInTheDocument();
+  });
+  it('Should match snapshot', () => {
+    const { container } = renderTheme(<Comp title='ola' />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
